6fe6796c3949b418169db5149dc30fe54a3b17a0
NikhilAnpat <111105422+NikhilAnpat@users.noreply.github.com>
2023-06-02 03:51:45 -0700

    Add files via upload

---
+++ Assignment/Day_4/Person/Student.java
@@ -0,0 +1,20 @@
+
+public class Student extends Person 
+{
+	private int m1,m2,m3;
+	
+	public Student() {
+		System.out.println("it is default constructor");
+	}
+	public Student(int pid,String pname,String pmob,int m1,int m2,int m3) {
+		super(pid,pname,pmob);
+		this.m1=m1;
+		this .m2=m2;
+		this.m3=m3;
+	}
+	
+	public String toString() {
+		return super.toString()+ "Subject 1: " +m1+ "  Subject 2 :"+m2+ "   Subject3:"+m3; 
+	}
+
+}
